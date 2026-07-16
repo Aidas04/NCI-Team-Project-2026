634dff9036dfd08dbdc89b5a364fe5331b0caafd
Ionut Ciobanu <ci.tiyko@gmail.com>
2026-07-16 15:05:26 +0100

    change some error causing docker settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-#   Edited by:
-#   Ionut Ciobanu
-#
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 exec "$@"
-ENTRYPOINT ["/app/entrypoint.sh"]
